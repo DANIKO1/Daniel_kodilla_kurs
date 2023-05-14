9cf1859cf2276eba56e0b4ea9202e9cc7de9e0b2
Daniel Koszerny <danielkoszerny@gmail.com>
2023-05-14 22:50:45 +0200

    6.2. Czym jest Test Driven Development (TDD)?

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/calculator/CalculatorTest.java
@@ -0,0 +1,12 @@
+package com.kodilla.testing.calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void main() {
+    }
+}
