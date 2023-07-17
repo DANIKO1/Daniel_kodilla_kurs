995f9d23c8ff555a65cfd99c1048251832f00e25
Daniel Koszerny <danielkoszerny@gmail.com>
2023-07-17 23:12:36 +0200

    Zadanie: Tworzymy encję list zadań

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,12 @@ repositories {
 }
 
 dependencies {
+    implementation("org.webjars.bower:RWD-Table-Patterns:5.2.5")
+	implementation("dev.the-fireplace:Erroring-Entity-Remover:1.0.2+1.18")
+	implementation("dev.the-fireplace:Erroring-Entity-Remover:1.0.2+1.18")
+	implementation("dev.the-fireplace:Erroring-Entity-Remover:1.0.2+1.18")
+	implementation("io.github.andriamarosoa:Entity:1.0.0")
+	implementation("org.webjars.bower:RWD-Table-Patterns:5.2.5")
 }
 
 tasks.withType<Test> {
