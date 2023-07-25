7003104c7edb85cbd5b3b3c6cd410774824c349b
Daniel Koszerny <danielkoszerny@gmail.com>
2023-07-25 21:46:19 +0200

    Podstawy JSON

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,22 +3,16 @@ plugins {
 }
 
 group = "com.kodilla"
-version = "0.0.1-SNAPSHOT"
+version = "0.0.1"
 
 repositories {
 	mavenCentral()
 }
 
 dependencies {
-    implementation("org.webjars.bower:RWD-Table-Patterns:5.2.5")
-	implementation("io.github.archimandrite:Entity:1.0.0")
-	implementation("org.webjars.bower:RWD-Table-Patterns:5.2.5")
-    implementation("au.com.dius:pact-jvm-provider-spring_2.12:3.6.15")
-    implementation("au.com.dius:pact-jvm-provider-spring_2.12:3.6.15")
-    implementation("au.com.dius:pact-jvm-provider-spring_2.12:3.6.15")
-	implementation("au.com.dius:pact-jvm-provider-spring_2.12:3.6.15")
-	implementation("au.com.dius:pact-jvm-provider-spring_2.12:3.6.15")
-	implementation("au.com.dius:pact-jvm-provider-spring_2.12:3.6.15")
+
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<Test> {
